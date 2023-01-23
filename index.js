a95dc07e58e5758099f4923a7676e8a6d617af0e
Edgar Martinez <edgarmartinez@Edgars-MBP.attlocal.net>
2023-01-22 20:49:59 -0600

    coded addRole()

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 require("dotenv").config();
-console.log(process.env);
 
 const db = mysql.createConnection(
   {
@@ -17,18 +16,21 @@ const db = mysql.createConnection(
 function viewDepartments() {
   db.query("SELECT * FROM department", function (err, results) {
     console.table(results);
+    init();
   });
 }
 
 function viewEmployees() {
   db.query("SELECT * FROM employee", function (err, results) {
     console.table(results);
+    init();
   });
 }
 
 function viewRoles() {
   db.query("SELECT * FROM role", function (err, results) {
     console.table(results);
+    init();
   });
 }
 
@@ -36,73 +38,137 @@ function addDepartment() {
   inquirer
     .prompt({
       type: "input",
-      message: "Enter department name",
-      name: "departmentName",
+      message: "Enter new department name",
+      name: "department",
     })
     .then((response) => {
-      console.log(`INSERT INTO department (name) VALUES (${response.departmentName})`);
-      return;
+      db.query("INSERT INTO department (name) VALUES (?)", response.department, function (err, results) {
+        console.log(results);
+      });
+      init();
+    });
+  // added department_name to the database
+}
+function addEmployee() {
+  // db.query("SELECT title from role", function (err, results) {
+  //   // console.log(typeof results, results);
+  //   const temp = [];
+  //   results.forEach((el) => {
+  //     temp.push(el.title);
+  //   });
+  //   temp.forEach((el) => {
+  //     roles.push(el);
+  //   });
+  // });
+  //
+  // enter employees first name
+  // enter employees last name
+  // what is the employees role? (list of roles)
+  // who is the employees manager? (list of managers)
+  // added (first_name last_name) to the database
+  // init();
+}
+function addRole() {
+  // enter name of role
+  // enter salary of role
+  // what department does the role belong to? (list of departments)
+  // added role_name to the database
+  db.query("SELECT * FROM department", function (err, results) {
+    const departments = results.map((department) => ({
+      name: department.name,
+      value: department.id,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Enter title of new role",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "Enter salary of new role",
+          name: "salary",
+        },
+        {
+          type: "list",
+          message: "What department does the role belong to?",
+          name: "department",
+          choices: departments,
+        },
+      ])
+      .then((response) => {
+        db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [response.title, response.salary, response.department], function (err, results) {
+          console.log(results);
+        });
+        console.log(response.title, response.salary, response.department);
+        init();
+      });
+  });
+}
+
+function updateEmployeeRole() {
+  // which employees rold do you want to update? (list of employees)
+  // which role do you want to assign the selected employee? (list of roles)
+  // updated employees role
+}
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          { name: "View all Employees", value: "VIEW EMPLOYEES" },
+          { name: "Add a new Employee", value: "ADD EMPLOYEE" },
+          { name: "Update Employee Role", value: "UPDATE EMPLOYEE ROLE" },
+          { name: "View all Roles", value: "VIEW ROLES" },
+          { name: "Add a new Role", value: "ADD ROLE" },
+          { name: "View all Departments", value: "VIEW DEPARTMENTS" },
+          { name: "Add a new Department", value: "ADD DEPARTMENT" },
+          // -- BONUS --
+          // update employee managers
+          // { name: "Update Employee Manager", value: "UPDATE MANAGER" },
+
+          // view employees by manager
+          // { name: "View Employees by Manager", value: "VIEW EMPLOYEE BY MANAGERS" },
+
+          // delete departments
+          // { name: "Delete Departments", value: "DELETE DEPARTMENTS" },
+
+          // delete roles
+          // { name: "Delete Roles", value: "DELETE ROLES" },
+
+          // delete employees
+          // { name: "Delete Employees", value: "DELETE EMPLOYEES" },
+
+          // view total utilized budged of a dept (sum of salaries of all emps dept)
+          // { name: "View the sum of salaires of all departments", value: "BONUS_RENAME" },
+          { name: "Exit", value: "EXIT" },
+        ],
+      },
+    ])
+    .then((response) => {
+      if (response.choice === "VIEW EMPLOYEES") viewEmployees();
+
+      if (response.choice === "ADD EMPLOYEE") addEmployee();
+
+      if (response.choice === "UPDATE EMPLOYEE ROLE") updateEmployeeRole();
+
+      if (response.choice === "VIEW ROLES") viewRoles();
+
+      if (response.choice === "ADD ROLE") addRole();
+
+      if (response.choice === "VIEW DEPARTMENTS") viewDepartments();
+
+      if (response.choice === "ADD DEPARTMENT") addDepartment();
+
+      if (response.choice === "EXIT") process.exit();
+      // init();
     });
 }
-function addEmployee() {}
-function addRole() {}
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "What would you like to do?",
-      name: "choice",
-      choices: [
-        { name: "View all Employees", value: "VIEW EMPLOYEES" },
-        { name: "Add a new Employee", value: "ADD EMPLOYEE" },
-        { name: "Update Employee Role", value: "UPDATE EMPLOYEE ROLE" },
-        { name: "View all Roles", value: "VIEW ROLES" },
-        { name: "Add a new Role", value: "ADD ROLE" },
-        { name: "View all Departments", value: "VIEW DEPARTMENTS" },
-        { name: "Add a new Department", value: "ADD DEPARTMENT" },
-        // -- BONUS --
-        // update employee managers
-        // { name: "Update Employee Manager", value: "UPDATE MANAGER" },
-
-        // view employees by manager
-        // { name: "View Employees by Manager", value: "VIEW EMPLOYEE BY MANAGERS" },
-
-        // delete departments
-        // { name: "Delete Departments", value: "DELETE DEPARTMENTS" },
-
-        // delete roles
-        // { name: "Delete Roles", value: "DELETE ROLES" },
-
-        // delete employees
-        // { name: "Delete Employees", value: "DELETE EMPLOYEES" },
-
-        // view total utilized budged of a dept (sum of salaries of all emps dept)
-        // { name: "View the sum of salaires of all departments", value: "BONUS_RENAME" },
-        { name: "Exit", value: "EXIT" },
-      ],
-    },
-  ])
-  .then((response) => {
-    if (response.choice === "VIEW EMPLOYEES") viewEmployees();
-
-    if (response.choice === "ADD EMPLOYEE") {
-    }
-
-    if (response.choice === "UPDATE EMPLOYEE ROLE") {
-    }
-
-    if (response.choice === "VIEW ROLES") viewRoles();
-
-    if (response.choice === "ADD ROLE") {
-    }
-
-    if (response.choice === "VIEW DEPARTMENTS") viewDepartments();
-
-    if (response.choice === "ADD DEPARTMENT") addDepartment();
-
-    if (response.choice === "EXIT") {
-      // prompt.ui.close();
-      // exit();
-    }
-  });
+init();
